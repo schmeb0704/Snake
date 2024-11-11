455a33bb58eb955634af4c8c7c0f5538589b3f2b
Sean Ben <seanauditor0704@gmail.com>
2024-11-12 05:56:33 +0800

    feat: added collision check and refactored variables

--- src/SnakeBody/SnakeBody.java
+++ src/SnakeBody/SnakeBody.java
@@ -1,33 +1,47 @@
 package SnakeBody;
 
 import java.awt.*;
+
+import Main.GamePanel;
+import Main.PlayManager;
 import Utils.KeyHandler;
 
 public class SnakeBody {
     public int x,y;
-    int movementSpeed = 8;
+    int size = 40;
+    int movementSpeed = 5;
+    boolean leftEdgeCollision, rightEdgeCollision, topEdgeCollision, bottomEdgeCollision;
 
     public void setXY(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    public void edgeCollisionCheck(){
+        leftEdgeCollision = rightEdgeCollision = topEdgeCollision = bottomEdgeCollision = false;
+
+        if(x+size == PlayManager.left_x){
+            leftEdgeCollision = true;
+        }
+
+    }
+
     public void update(){
-        if(KeyHandler.downPressed){
-            y += movementSpeed;
-            KeyHandler.downPressed = false;
-        }
-        if(KeyHandler.upPressed){
-            y -= movementSpeed;
-            KeyHandler.upPressed = false;
-        }
-        if(KeyHandler.rightPressed){
-            x += movementSpeed;
-            KeyHandler.rightPressed = false;
-        }
-        if(KeyHandler.leftPressed){
-            x -= movementSpeed;
-            KeyHandler.leftPressed = false;
+        edgeCollisionCheck();
+
+        if(!leftEdgeCollision && !rightEdgeCollision && !topEdgeCollision && !bottomEdgeCollision ){
+            if(KeyHandler.downPressed){
+                y += movementSpeed;
+            }
+            if(KeyHandler.upPressed){
+                y -= movementSpeed;
+            }
+            if(KeyHandler.rightPressed){
+                x += movementSpeed;
+            }
+            if(KeyHandler.leftPressed){
+                x -= movementSpeed;
+            }
         }
     }
 
@@ -35,7 +49,7 @@ public class SnakeBody {
 
     public void drawBody(Graphics2D g){
         g.setColor(Color.blue);
-        g.fillRect(x, y, 40, 40);
+        g.fillRect(x, y, size, size);
 
     }
 }
