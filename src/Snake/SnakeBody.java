e937c42b7473534c29d9e1c1efcfaa336eeed9c2
Sean Ben <seanauditor0704@gmail.com>
2024-11-13 05:22:43 +0800

    feat: make food randomly change position when colliding with snake body

--- src/Snake/SnakeBody.java
+++ src/Snake/SnakeBody.java
@@ -1,13 +1,12 @@
-package SnakeBody;
+package Snake;
 
 import java.awt.*;
 
 import Main.GamePanel;
-import Main.PlayManager;
 import Utils.KeyHandler;
 
 public class SnakeBody {
-    public int x,y;
+    public int x,y, right_x, bottom_y;
     int size = 40;
     int movementSpeed = 5;
     boolean leftEdgeCollision, rightEdgeCollision, topEdgeCollision, bottomEdgeCollision;
@@ -15,17 +14,19 @@ public class SnakeBody {
     public void setXY(int x, int y){
         this.x = x;
         this.y = y;
+        right_x = x + size;
+        bottom_y = y + size;
     }
 
     public void edgeCollisionCheck(){
         leftEdgeCollision = rightEdgeCollision = topEdgeCollision = bottomEdgeCollision = false;
 
         if(x+size == GamePanel.WIDTH){
-            leftEdgeCollision = true;
+            rightEdgeCollision = true;
         }
 
         if(x == 0){
-            rightEdgeCollision = true;
+            leftEdgeCollision = true;
         }
         if(y == GamePanel.HEIGHT-50){
             bottomEdgeCollision = true;
@@ -35,21 +36,33 @@ public class SnakeBody {
         }
     }
 
+    public void checkFoodCollision(){
+        if(SnakeFood.x >= x && SnakeFood.x <= right_x && SnakeFood.y < bottom_y && SnakeFood.y > y){
+            SnakeFood.isEaten = true;
+            SnakeFood.updateCoordinates();
+        }
+    }
+
     public void update(){
         edgeCollisionCheck();
+        checkFoodCollision();
 
         if(!leftEdgeCollision && !rightEdgeCollision && !topEdgeCollision && !bottomEdgeCollision ){
             if(KeyHandler.downPressed){
                 y += movementSpeed;
+                bottom_y += movementSpeed;
             }
             if(KeyHandler.upPressed){
                 y -= movementSpeed;
+                bottom_y -= movementSpeed;
             }
             if(KeyHandler.rightPressed){
                 x += movementSpeed;
+                right_x += movementSpeed;
             }
             if(KeyHandler.leftPressed){
                 x -= movementSpeed;
+                right_x -= movementSpeed;
             }
         }
     }
--- src/Snake/SnakeFood.java
+++ src/Snake/SnakeFood.java
@@ -1,14 +1,25 @@
 package Snake;
 
+import Main.GamePanel;
+
 import java.awt.*;
 
 public class SnakeFood {
-    int x, y;
-    int size = 30;
+    public static int x, y;
+    static int size = 20;
+    public static boolean isEaten = false;
 
     public void setCoordinates(int x, int y){
-        this.x = x;
-        this.y = y;
+        SnakeFood.x = x;
+        SnakeFood.y = y;
+    }
+
+    public static void updateCoordinates(){
+        int new_x = (int) Math.floor(Math.random() * GamePanel.WIDTH);
+        int new_y = (int) Math.floor(Math.random() * GamePanel.HEIGHT);
+
+        x = new_x;
+        y = new_y;
     }
 
     public void drawFood(Graphics2D g){
