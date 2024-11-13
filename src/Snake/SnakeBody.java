137e0416f81f9754bb557716db1abded57bcdc0f
Sean Ben <seanauditor0704@gmail.com>
2024-11-14 04:29:28 +0800

    feat: draw body segments

--- src/Snake/SnakeBody.java
+++ src/Snake/SnakeBody.java
@@ -1,6 +1,7 @@
 package Snake;
 
 import java.awt.*;
+import java.security.Key;
 import java.util.ArrayList;
 
 import Main.GamePanel;
@@ -14,19 +15,20 @@ public class SnakeBody {
     boolean leftEdgeCollision, rightEdgeCollision, topEdgeCollision, bottomEdgeCollision;
     public static ArrayList<SnakeSegment> segments = new ArrayList<>();
     SnakeSegment head;
+    SnakeSegment tail;
 
     public SnakeBody(){
         segments.add(new SnakeSegment());
-        segments.add(new SnakeSegment());
         head = segments.getFirst();
+        tail = segments.getLast();
     }
 
 
     public void setXY(int x, int y){
-        segments.getFirst().x = x;
-        segments.getFirst().y = y;
-        segments.getFirst().right_x = x + SnakeSegment.size;
-        segments.getFirst().bottom_y = x + SnakeSegment.size;
+       head.x = x;
+       head.y = y;
+       head.right_x = x + SnakeSegment.size;
+       head.bottom_y = y + SnakeSegment.size;
     }
 
     public void edgeCollisionCheck(){
@@ -47,10 +49,42 @@ public class SnakeBody {
         }
     }
 
+    public void addSegment(){
+        SnakeSegment nextSegment = new SnakeSegment();
+        if(KeyHandler.upPressed){
+            nextSegment.x = tail.x;
+            nextSegment.y = tail.bottom_y;
+            nextSegment.bottom_y = nextSegment.y + SnakeSegment.size;
+            nextSegment.right_x = nextSegment.x + SnakeSegment.size;
+        }
+        if(KeyHandler.downPressed){
+            nextSegment.x = tail.x;
+            nextSegment.y = tail.x + SnakeSegment.size;
+            nextSegment.bottom_y = nextSegment.y + SnakeSegment.size;
+            nextSegment.right_x = nextSegment.x + SnakeSegment.size;
+        }
+
+        if(KeyHandler.leftPressed){
+            nextSegment.x = tail.right_x;
+            nextSegment.y = tail.y;
+            nextSegment.bottom_y = nextSegment.y + SnakeSegment.size;
+            nextSegment.right_x = nextSegment.x + SnakeSegment.size;
+        }
+        if(KeyHandler.rightPressed){
+            nextSegment.x = tail.x - SnakeSegment.size;
+            nextSegment.y = tail.y;
+            nextSegment.bottom_y = nextSegment.y + SnakeSegment.size;
+            nextSegment.right_x = nextSegment.x + SnakeSegment.size;
+        }
+        segments.add(nextSegment);
+        tail = nextSegment;
+    }
+
     public void checkFoodCollision(){
-        if(SnakeFood.x >= head.x && SnakeFood.x <= head.right_x && SnakeFood.y < head.bottom_y && SnakeFood.y > head.y){
+        if(SnakeFood.x >= head.x && SnakeFood.x <= head.right_x && SnakeFood.y <= head.bottom_y && SnakeFood.y >= head.y){
             SnakeFood.isEaten = true;
             SnakeFood.updateCoordinates();
+            addSegment();
         }
     }
 
@@ -92,9 +126,12 @@ public class SnakeBody {
 
     public void drawBody(Graphics2D g){
         g.setColor(Color.blue);
-//        g.fillRect(x, y, size, size);
-        g.fillRect(segments.getFirst().x, segments.getFirst().y, SnakeSegment.size, SnakeSegment.size );
-        g.fillRect(head.x, head.y,  SnakeSegment.size, SnakeSegment.size );
+//        g.fillRect(head.x, head.y,  SnakeSegment.size, SnakeSegment.size );
+
+        System.out.println(segments.size());
+        for (SnakeSegment segment : segments){
+            g.fillRect(segment.x, segment.y, SnakeSegment.size, SnakeSegment.size);
+        }
 
     }
 }
